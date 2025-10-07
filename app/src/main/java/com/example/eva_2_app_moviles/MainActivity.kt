70dac41c45f614de77c9c0544acf1613f5813081
Viicttorr <victor.alarcon29@inacapmail.cl>
2025-10-07 00:58:24 -0300

    Pantalla de Recuperacion Pass 99%

--- app/src/main/java/com/example/eva_2_app_moviles/MainActivity.kt
+++ app/src/main/java/com/example/eva_2_app_moviles/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.ImageButton
+import android.widget.Toast
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        findViewById<ImageButton>(R.id.btnForgot).setOnClickListener {
+            val intent = Intent(this, RecuperaClave::class.java)
+            startActivity(intent)
         }
     }
 }
